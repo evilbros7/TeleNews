a8adc0da77410810aa57d7c3efa021c4fc9b4803
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-06 20:08:52 +0530

    Update gsmarena.py

--- sites/gsmarena.py
+++ sites/gsmarena.py
@@ -5,7 +5,7 @@ def ndtv():
 	url = 'https://www.gsmarena.com/news.php3'
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
-	cl = soup.findAll(class_=news-item')
+	cl = soup.findAll(class_='news-item')
 	
 	List = []
 	count=0 #to get only top 15 news
