2dcb97dba6346b7dc45a9ac10321286fa2c29043
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-06 20:07:36 +0530

    Update gsmarena.py

--- sites/gsmarena.py
+++ sites/gsmarena.py
@@ -1,19 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
 
-def arena():
+def ndtv():
 	url = 'https://www.gsmarena.com/news.php3'
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
-	cl = soup.findAll(class_='news-item')
-
-	#print(cl[1])
-	#print(cl[1].a.get('title'))
-	List = []
+	cl = soup.findAll(class_=news-item')
 	
+	List = []
 	count=0 #to get only top 15 news
 	for i in cl:
-		#print(i.a.get('title'))
+		#print(i.text)
 		count=count+1
 		if(count==15):
 			break
@@ -21,6 +18,5 @@ def arena():
 			#List.append("\n\n🌐 Join @pvxtechnews for daily tech news !")
 
 		List.append("\n\n🔅")
-		List.append(i.a.get('title'))
+		List.append(i.text)
 	return List
-
