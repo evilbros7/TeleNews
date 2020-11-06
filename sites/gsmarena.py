a1f2cf963cd3dc6c3f38f8bc2387a975d7d0e0d0
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-06 20:11:39 +0530

    Update gsmarena.py

--- sites/gsmarena.py
+++ sites/gsmarena.py
@@ -6,11 +6,14 @@ def arena():
 	page = requests.get(url)
 	soup = BeautifulSoup(page.content, 'html.parser')
 	cl = soup.findAll(class_='news-item')
-	
+
+	#print(cl[1])
+	#print(cl[1].a.get('title'))
 	List = []
+	
 	count=0 #to get only top 15 news
 	for i in cl:
-		#print(i.text)
+		#print(i.a.get('title'))
 		count=count+1
 		if(count==15):
 			break
@@ -18,5 +21,6 @@ def arena():
 			#List.append("\n\n🌐 Join @pvxtechnews for daily tech news !")
 
 		List.append("\n\n🔅")
-		List.append(i.text)
+		List.append(i.a.get('title'))
 	return List
+
