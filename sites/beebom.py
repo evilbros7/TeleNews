e961ae5851bd6231454af08fd664398f30723010
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-06 16:11:59 +0530

    Add files via upload

---
+++ sites/beebom.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+def bom():
+	url = 'https://beebom.com/category/news/'
+	headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+	page = requests.get(url, headers=headers)
+
+	#page = requests.get(url)
+
+	#print(page)
+	soup = BeautifulSoup(page.content, 'html.parser')
+	#print(soup)
+	cl = soup.findAll(class_='bee-list')
+
+	#print(cl)
+	count=0
+	List = []
+	for i in cl:
+		count=count+1
+		if count==15:
+			break
+		#print(i.text)
+		x=i.text.find("\n\n\n")
+		#print(x)
+		List.append("\n\n🌐")
+		List.append(i.text[3:x])
+
+	return List
+	
