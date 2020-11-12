9587fad63bdf71d294539eb8cdaf8a373288eae5
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-12 21:37:08 +0530

    Update xda.py

--- sites/xda.py
+++ sites/xda.py
@@ -2,20 +2,27 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 def xdaa():
-	url="https://www.xda-developers.com/category/xda-news/"
-	response=requests.get(url)
-		
+ url="https://www.xda-developers.com/category/xda-news/"
+ response=requests.get(url)
+  
 
-	soup=bs(response.content,"html.parser")
-	headings=soup.findAll("h4")
+ soup=bs(response.content,"html.parser")
+ headings=soup.findAll("div",class_="item_content")
 
-	count=0
-	newsList=[]
-	for heading in headings:
-		count+=1
-		newsList.append("\n\n🌐")
-		newsList.append(heading.text)
-		if count==15:
-			break
-
-	return newsList
+ count=0
+ newsList=[]
+ for heading in headings:
+  count+=1
+  newsList.append("\n\n🌐")
+  newsList.append(heading.a.text)
+  if count==15:
+   break
+   
+ return newsList
+ 
+List=xdaa()
+#print(List)
+List.insert(0,'☆☆☆☆☆💥 Tech News 💥☆☆☆☆☆')
+#print(List)
+text = " ".join(List)
+print(text)
