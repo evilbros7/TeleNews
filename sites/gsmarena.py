aef5e47f393dd5cd88f9cc393ddd1b7a10981e3d
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-13 10:41:00 +0530

    Update gsmarena.py

--- sites/gsmarena.py
+++ sites/gsmarena.py
@@ -1,10 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-def arena():
+def gsm():
 	url="https://www.gsmarena.com/news.php3"
 	response=requests.get(url)
-		
 
 	soup=bs(response.content,"html.parser")
 	headings=soup.findAll("h3")
@@ -13,9 +12,14 @@ def arena():
 	newsList=[]
 	for heading in headings:
 		count+=1
-		newsList.append("\n\n🌐")
-		newsList.append(heading.text)
+
 		if count==15:
 			break
+
+		#if count==11:
+			#List.append("\n\n🌐 Join @premiumcoursesdrive for daily tech news !")
+		
+		newsList.append("\n\n🌐")
+		newsList.append(heading.text)
 
 	return newsList
