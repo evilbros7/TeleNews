efad70a614840d72eac8bf76ba2345f3667306f4
evilbros7 <69081291+evilbros7@users.noreply.github.com>
2020-11-13 11:09:12 +0530

    Update xda.py

--- sites/xda.py
+++ sites/xda.py
@@ -1,28 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-def xdaa():
- url="https://www.xda-developers.com/category/xda-news/"
- response=requests.get(url)
-  
+def xda():
+    url="https://www.xda-developers.com/category/xda-news/"
+    response=requests.get(url)
+    
+    soup=bs(response.content,"html.parser")
+    subHtmlBlock=soup.find("div",class_="col main-content col_9_of_12")
+    headings=subHtmlBlock.findAll("div",class_="item_content")
+    count=0
+    
+    newsList=[]
+    for heading in headings:
+        count+=1
+        if count==15:
+            break
+		#if count==11:
+			#List.append("\n\n🌐 Join @premiumcoursesdrive for daily tech news !")
 
- soup=bs(response.content,"html.parser")
- headings=soup.findAll("div",class_="item_content")
+        newsList.append("\n\n💠")
+        newsList.append(heading.a.text)
 
- count=0
- newsList=[]
- for heading in headings:
-  count+=1
-  newsList.append("\n\n🌐")
-  newsList.append(heading.a.text)
-  if count==30:
-   break
-   
- return newsList
- 
-List=xdaa()
-#print(List)
-List.insert(0,'☆☆☆☆☆💥 Tech News 💥☆☆☆☆☆')
-#print(List)
-text = " ".join(List)
-print(text)
+    return newsList
